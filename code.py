0a726c553a0dbd5149bf2ec0ced49598467ab93b
Shivesh08 <89000498+Shivesh08@users.noreply.github.com>
2023-03-11 10:46:06 +0530

    Update code.py

--- code.py
+++ code.py
@@ -3,9 +3,9 @@ import json
 import time
 from discord_webhook import DiscordWebhook
 
-id="UCFPyFfzaO3U0XRG_DgnIjsw"
+id="CHANNEL_ID"
 
-key="AIzaSyBR6YZQFmm3Wj2QWch1-r1QIbWenZwoIms"
+key="YOUTUBE_API_KEY"
 
 x=0
 
@@ -21,6 +21,6 @@ while(x<1):
    subsb=json.loads(data)["items"][0]["statistics"]["subscriberCount"]
 
    if subsb>subsa:
-      webhook = DiscordWebhook(url='https://discord.com/api/webhooks/1083812337816973424/QolP3zaEzE-f62uq0WjwiUh6l6m3hOPMYVIAFG6lP6gkfihyw-jdXlHAbklf51VGSxOL', content='Your Youtube subscriber count increased by 1!')
+      webhook = DiscordWebhook(url='WEBHOOK_URL', content='Your Youtube subscriber count increased by 1!')
       response = webhook.execute()
       print("Your subscriber count increased by 1!")
