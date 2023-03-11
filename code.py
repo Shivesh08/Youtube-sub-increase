c5806aca07d8f01dabee2a88d5e5e96f0c3c8dbe
Shivesh08 <89000498+Shivesh08@users.noreply.github.com>
2023-03-11 12:39:36 +0530

    Update code.py

--- code.py
+++ code.py
@@ -2,6 +2,7 @@ import urllib.request
 import json
 import time
 from discord_webhook import DiscordWebhook
+from discord import SyncWebhook
 
 id="CHANNEL_ID"
 
@@ -13,14 +14,22 @@ while(x<1):
     
    data = urllib.request.urlopen("https://www.googleapis.com/youtube/v3/channels?part=statistics&id="+id+"&key="+key).read()
    subsa=json.loads(data)["items"][0]["statistics"]["subscriberCount"]
-
-
+   print(subsa)
+   
    time.sleep(60)
 
    data = urllib.request.urlopen("https://www.googleapis.com/youtube/v3/channels?part=statistics&id="+id+"&key="+key).read()
    subsb=json.loads(data)["items"][0]["statistics"]["subscriberCount"]
+   print(subsb)
 
+   increase = int(subsb) - int(subsa)
+   decrease = int(subsa) - int(subsb)
+   
    if subsb>subsa:
-      webhook = DiscordWebhook(url='WEBHOOK_URL', content='Your Youtube subscriber count increased by 1!')
-      response = webhook.execute()
-      print("Your subscriber count increased by 1!")
+      webhook = SyncWebhook.from_url("WEBHOOK_URL")
+      webhook.send(f"Your Youtube subscriber count increased by {increase}")
+      print("Your subscriber count increased by", increase)
+   elif subsa>subsb:
+     webhook = SyncWebhook.from_url("WEBHOOK_URL")
+     webhook.send(f"Your Youtube subscriber count decreased by {decrease}")
+     print("Your subscriber count decreased by", decrease)
